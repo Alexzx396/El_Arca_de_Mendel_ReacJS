f831684301ddca0ace78c56ba53192f6fb0239a9
alexarce <a.l.e.x_91@hotmail.com>
2021-11-23 21:39:43 -0300

    firestore1

---
+++ src/services/GetFirestore.jsx
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjLUcev4NLc4z-9DoOQZbq4BG0zWiteKk",
+  authDomain: "proyecto-react-semillas.firebaseapp.com",
+  projectId: "proyecto-react-semillas",
+  storageBucket: "proyecto-react-semillas.appspot.com",
+  messagingSenderId: "1081397938386",
+  appId: "1:1081397938386:web:013a7845e2c239be0a450b"
+};
+
+// Initialize Firebase
+const catalogue = firebase.initializeApp(firebaseConfig);
+
+export function getFirestore(){    
+    return firebase.firestore(catalogue)
+}
